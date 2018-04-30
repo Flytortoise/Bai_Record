5ea8b07dc2926e54a4b7e7c10f90cbd11ba8493e
Flytortoise <3286114036@qq.com>
2018-04-30 14:53:58 +0800

    1.1:client update

--- Record1_1/client/code/recordwidget.cpp
+++ Record1_1/client/code/recordwidget.cpp
@@ -70,11 +70,8 @@ void RecordWidget::CreateInit()
                 _startBtn->setEnabled(false);
                 _endBtn->setEnabled(true);
              #ifdef TOFILE //存到文件
-                QString fileName = QString("./%1-%2.wav").arg(
-                    QDateTime::currentDateTime().toString("yyyy-MM-dd"),
-                    QTime::currentTime().toString("hh-mm-ss"));
 
-                outFile.setFileName(qApp->applicationDirPath() + fileName); //语音原始文件
+                outFile.setFileName(qApp->applicationDirPath() + QString("/test.wav")); //语音原始文件
                 outFile.open(QIODevice::WriteOnly | QIODevice::Truncate);
                 _audioInput->start(&outFile);
                 if(!flag)
